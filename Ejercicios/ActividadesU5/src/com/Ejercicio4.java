f8b6ddad56fea7a6720e982e514c777c4afd09b1
Pablo <jdommay863@g.educaand.es>
2025-10-31 17:56:07 +0100

    Ejercicio 4 U5

--- Ejercicios/ActividadesU5/src/com/Ejercicio4.java
+++ Ejercicios/ActividadesU5/src/com/Ejercicio4.java
@@ -1,4 +1,8 @@
 package com;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Implementar un programa que inicialice una tabla con tus números favoritos. 
  * Pedir al usuario el índice de un elemento que será eliminado.
@@ -12,10 +16,39 @@ package com;
  */
 public class Ejercicio4 {
       public static void main(String [] args) {
+             Scanner sc = new Scanner(System.in);                                                                                                                                                                                                                                                                                                                                       
             int [] numeros = {2, 5, 13, 6, 9, 10};
-            
+            int aux;
+           
+            System.out.println("Inicio: ");
+            mostrarTabla(numeros);
+           
             do {
+             //Pedir al usuario el índice de un elemento
+              System.out.println("Dime el indice: ");
+              int indice = sc.nextInt();
+              
+              if(indice>=numeros.length || indice < 0 ) {
+                  continue;                                                                                                                                                                                                                                                                                                                                         
+              }
+              // Guardamos el último número de la tabla
+              aux = numeros[numeros.length -1];
+              numeros[indice] = aux;
+              numeros = Arrays.copyOf(numeros, numeros.length -1);
+             
+              
+              mostrarTabla(numeros);
                                                                                                                                                                                                                                                                                                                                                           
             }while(numeros.length>0);
+     
+            System.out.println("FIN");
+      }
+      
+      public static void mostrarTabla(int [] tabla) {
+         System.out.print( "[ ");                                                                                                                                                                                                                                                                                                                                      
+           for(int elemento : tabla) {
+               System.out.print(elemento +"\t");                                                                                                                                                                                                                                                                                                                                          
+           }
+           System.out.print( "]");          
       }
 }
