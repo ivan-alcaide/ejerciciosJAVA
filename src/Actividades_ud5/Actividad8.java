2fafe945031b3dc7f460bed04f772f4025c6587e
linuxlite <alcaideivan00@gmail.com>
2025-11-07 18:45:46 +0100

    Activiad 8 mal

--- src/Actividades_ud5/Actividad8.java
+++ src/Actividades_ud5/Actividad8.java
@@ -8,34 +8,52 @@ public class Actividad8 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 
-		int[] tabla=new int[1];
+		int[] tabla=new int[0];
 		
 		System.out.println(Arrays.toString(sinRepetidos(tabla)));
 		
 	}
 	
 	
-	public static int[] sinRepetidos(int t[]) {
+	public static int[] sinRepetidos(int tabla[]) {
 		Scanner sc=new Scanner(System.in);
 
-		int[] tabla=new int[0];
-		int temp, posicion;
+		
+		int temp, posicion, orig, sumj=1;
 		
 		
 		System.out.println("A continuacion podra añadir numeros a una tabla, cuando quiera parar escriba -1");
 		do {
 		System.out.println("Introduzca un numero para añadir a la tabla");
 		temp=sc.nextInt();
-		if(temp!=-1) {
-			Arrays.copyOf(tabla, tabla.length+1);
-			posicion=tabla.length-1;
-			tabla[posicion]=temp;
+		if(temp==-1) {
+			break;
 		}
+		tabla=Arrays.copyOf(tabla, tabla.length+1);
+		posicion=tabla.length-1;
+		tabla[posicion]=temp;
 		
-			
+	
 			
 		}while(temp!=-1);
 		
+		for(int i=0;i<tabla.length;i++) {
+			
+			
+			for(int j=sumj;j<tabla.length;j++){
+				
+				if(tabla[i]==tabla[j]) {
+					orig=tabla[tabla.length-1];
+					tabla[tabla.length-1]=tabla[j];
+					tabla[j]=orig;
+					tabla=Arrays.copyOf(tabla, tabla.length - 1);
+					
+				}
+				
+			}
+			sumj++;
+		}
+		System.out.println("La tabla sin numeros repetidos es: \n");
 		return tabla;
 			
 		
