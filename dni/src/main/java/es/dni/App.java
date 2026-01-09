02db76fd5033e4393194a6d608c409a833f60b3d
linuxlite <alcaideivan00@gmail.com>
2026-01-09 17:03:36 +0100

    dni

---
+++ dni/src/main/java/es/dni/App.java
@@ -0,0 +1,20 @@
+package es.dni;
+
+import com.github.javafaker.Faker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( Faker.instance().pokemon().name());
+        
+        ControladorDNI controlador=new ControladorDNI();
+        
+    }
+    
+    
+}
