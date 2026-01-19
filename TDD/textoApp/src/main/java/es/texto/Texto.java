a4bd5c6bceddd7ace6c1e61a0010ae2c0ddde291
Pablo <pablo.dominguez@iescamas.es>
2026-01-19 16:48:03 +0100

    tdd

---
+++ TDD/textoApp/src/main/java/es/texto/Texto.java
@@ -0,0 +1,15 @@
+package es.texto;
+/**
+ * Notas rápidas para que el alumnado haga TDD “bien”
+
+Empieza implementando constructores + getters + longitud().
+
+Luego anadirFinal(char) y anadirInicio(char).
+
+Después anadirFinal(String) y anadirInicio(String) con regla “todo o nada” (si no cabe, no modifica).
+
+Finalmente contarVocales().
+ */
+public class Texto {
+
+}
